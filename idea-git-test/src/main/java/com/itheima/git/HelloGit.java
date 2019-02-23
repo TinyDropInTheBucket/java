7afd77b811e6b9e5d3122a3d392d9e5cd6d0772f
DropInBucket <fzhliang007@163.com>
2019-02-23 22:11:19 +0800

    添加test工程至git

---
+++ idea-git-test/src/main/java/com/itheima/git/HelloGit.java
@@ -0,0 +1,9 @@
+package com.itheima.git;
+
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello,Git!");
+		System.out.println("我自横刀向天笑");
+		System.out.println("去留肝胆两昆仑");
+	}
+}
